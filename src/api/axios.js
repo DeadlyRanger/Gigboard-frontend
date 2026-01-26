3cf5c917fcacdbcba1466969f67a85319e4588db
Deadlyranger <muditkubrey@gmail.com>
2026-01-27 00:11:58 +0530

    integration addded

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
-  withCredentials: true   // 🔥 THIS IS THE KEY
+  baseURL: "https://gigboard-backend-ox4r.onrender.com/api",
+  withCredentials: true  
 });
 
 export default api;
